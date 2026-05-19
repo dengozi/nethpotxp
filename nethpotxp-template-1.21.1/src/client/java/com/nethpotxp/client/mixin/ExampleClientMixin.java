837690463fd7cb35a39848db92618363226f51ae
Ata <atadrink@gmail.com>
2026-05-19 22:00:06 +0300

    Update ExampleClientMixin.java

--- nethpotxp-template-1.21.1/src/client/java/com/nethpotxp/client/mixin/ExampleClientMixin.java
+++ nethpotxp-template-1.21.1/src/client/java/com/nethpotxp/client/mixin/ExampleClientMixin.java
@@ -1,15 +1,15 @@
 package com.nethpotxp.client.mixin;
 
-import net.minecraft.client.Minecraft;
+import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Minecraft.class)
+@Mixin(MinecraftClient.class)
 public class ExampleClientMixin {
-	@Inject(at = @At("HEAD"), method = "run")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of Minecraft.run()V
-	}
+    @Inject(at = @At("HEAD"), method = "init")
+    private void init(CallbackInfo info) {
+        // İstemci (Client) başladığında çalışacak örnek kod alanı
+    }
 }
